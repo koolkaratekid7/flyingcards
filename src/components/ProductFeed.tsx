97e99ee600317b5a2be75ef48d4082c5e5c3f24b
thereconjacob <jvpokeyjake@gmail.com>
2023-07-31 15:26:00 +0100

    Add pagination for products

--- src/components/ProductFeed.tsx
+++ src/components/ProductFeed.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IProduct } from "../../typings";
 import Product from "./Product";
 
@@ -6,20 +7,59 @@ type Props = {
 };
 
 const ProductFeed = ({ products }: Props) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 12;
+
+  // Get current products
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  // Change page
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
   return (
-    <div className="flex flex-wrap justify-center -mt-10 md:-mt-52 lg:-mt-75 xl:-mt-80 mx-auto">
-      {products.slice(0, 4).map((product) => (
-        <Product key={product.id.toString()} product={product} />
-      ))}
-
-      {products.slice(4, 5).map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-
-      {products.slice(5, products.length).map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-wrap justify-center -mt-10 md:-mt-52 lg:-mt-75 xl:-mt-80 mx-auto">
+        {currentProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+      <p className="text-center">
+        Page {currentPage} of {Math.ceil(products.length / productsPerPage)}
+      </p>
+      <nav>
+        <ul className="pagination">
+          {[...Array(Math.ceil(products.length / productsPerPage)).keys()].map((x, i, arr) => (
+            <li key={x + 1} className="page-item">
+              <a onClick={() => paginate(x + 1)} className="page-link">
+                {x + 1}
+              </a>
+              {i !== arr.length - 1 && ", "}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <style jsx>{`
+        .pagination {
+          display: flex;
+          list-style: none;
+          margin: 0;
+          padding: 0;
+          justify-content: center;
+        }
+        .page-item {
+          margin-right: 0.25rem;
+        }
+        .page-link {
+          color: blue;
+          cursor: pointer;
+        }
+        .page-link:visited {
+          color: purple;
+        }
+      `}</style>
+    </>
   );
 };
 
