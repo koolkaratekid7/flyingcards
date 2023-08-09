a69a5be683b31d99c749e1e373ff9a0d87879ee6
thereconjacob <jvpokeyjake@gmail.com>
2023-08-09 14:10:26 +0100

    fix orders conditional check

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -29,15 +29,17 @@ const Orders = ({}: Props) => {
           Your Orders
         </h1>
         {session ? (
-          <h2>{orders.length} Orders</h2>
+          <>
+            <h2>{orders.length} Orders</h2>
+            <div className="mt-5 space-y-4">
+              {orders?.map((order) => (
+                <Order key={order.id} order={order} />
+              ))}
+            </div>
+          </>
         ) : (
           <h2>Please sign in to see your orders</h2>
         )}
-        <div className="mt-5 space-y-4">
-          {orders?.map((order) => (
-            <Order key={order.id} order={order} />
-          ))}
-        </div>
       </main>
     </div>
   );
